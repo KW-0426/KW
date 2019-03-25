e842323535c0209cc4f1a63641af02ae6023c3c7
KW-0426 <1157371273@qq.com>
2019-03-25 09:18:00 +0800

    add

---
+++ PHPDailyTest/week2.php
@@ -0,0 +1,46 @@
+<?php 
+// $num = [1,2,3,4];
+// 	function num($num)
+// 	{
+// 		for($i=1;$i<=4;$i++)
+// 		{
+// 			for($j=1;$j<=4;$j++)
+// 			{
+// 				for($n=1;$n<=4;$n++)
+// 				{
+// 					if($i != $j && $j != $n && $i != $n)
+// 					{
+// 						echo "$i$j$n</br>";
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return num($num);
+$aPath= "/a/b/c/d/test.php";
+$bPath= "/a/b/d/c/test.php";
+function findCommonPath($aPath,$bPath)
+{
+	
+}
+
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	距离2020年1月1日还剩<span id="day">天</span><span id="hour">时</span><span id="f">分</span><span id="m">秒</span>
+</body>
+</html>
+<script src="../jquery-1.10.2.js"></script>
+<script type="text/javascript">
+	function('#day').val();
+</script>
